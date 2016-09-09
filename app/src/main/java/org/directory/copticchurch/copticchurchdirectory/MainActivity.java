e875b652c7660087511739b70b74ea182c45f191
Abanoub <penanko@gmail.com>
2016-09-09 00:13:59 -0700

    Commit

--- app/src/main/java/org/directory/copticchurch/copticchurchdirectory/MainActivity.java
+++ app/src/main/java/org/directory/copticchurch/copticchurchdirectory/MainActivity.java
@@ -1,18 +1,32 @@
 package org.directory.copticchurch.copticchurchdirectory;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+        //Action bar
+        getSupportActionBar().setTitle(R.string.title);
+        //webview for Coptic Church Directory Site
         mywebView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -28,5 +42,34 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    } @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        return true;
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return false;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+
+
+
+        }
+
+    }
+
+
+
 }
---
+++ app/src/main/java/org/directory/copticchurch/copticchurchdirectory/Splash.java
@@ -0,0 +1,32 @@
+package org.directory.copticchurch.copticchurchdirectory;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+      Thread mythread = new Thread(){
+          @Override
+          public void run(){
+              try {
+                  sleep(3500);
+                  Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                  startActivity(intent);
+                  finish();
+              }catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+      };
+        mythread.start();
+
+
+    }
+}
